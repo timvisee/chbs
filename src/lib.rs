2aadf958ae73daae2a72c5d18949aa1cec7e9e40
timvisee <timvisee@gmail.com>
2018-08-01 22:48:36 +0200

    Create word sampler, which produces an infinite number of random words

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,12 @@
+extern crate rand;
+
+use rand::{
+    distributions::Uniform,
+    prelude::*,
+    rngs::ThreadRng,
+    thread_rng,
+};
+
 /// A static wordlist to use.
 const WORDLIST: &'static str = include_str!("../res/eff_large_wordlist.txt");
 
@@ -7,7 +16,7 @@ const WORDLIST: &'static str = include_str!("../res/eff_large_wordlist.txt");
 /// The included wordlist is prefixed with dice numbers, each line ends with
 /// an actual word. This function grabs the last alphabetic word from each
 /// line which are then collected into a vector.
-fn words<'a>() -> Vec<&'a str> {
+pub fn words<'a>() -> Vec<&'a str> {
     WORDLIST
         .lines()
         .filter_map(|line| line
@@ -18,9 +27,56 @@ fn words<'a>() -> Vec<&'a str> {
         .collect()
 }
 
+/// Build a word sampler which is an iterator that randomly samples words from
+/// the included wordlist.
+/// The word sampler is concidered cryptographically secure.
+pub fn word_sampler<'a>() -> WordSampler<'a> {
+    WordSampler::new(words())
+}
+
+/// A word sampler iterator that provides random words from a given wordlist.
+/// The randomization is concidered cryptographically secure.
+///
+/// As much words as needed may be pulled from this iterator.
+pub struct WordSampler<'a> {
+    /// List of words that is used for sampling.
+    words: Vec<&'a str>,
+
+    /// Random distribution used for sampling.
+    distribution: Uniform<usize>,
+
+    /// Random number generator used for sampling.
+    rng: ThreadRng,
+}
+
+impl<'a> WordSampler<'a> {
+    /// Build a new word sampler which samples the given word list.
+    pub fn new(words: Vec<&'a str>) -> WordSampler<'a> {
+        WordSampler {
+            distribution: Uniform::new(0, words.len()),
+            words,
+            rng: thread_rng(),
+        }
+    }
+}
+
+impl<'a> Iterator for WordSampler<'a> {
+    type Item = &'a str;
+
+    /// Sample the next random word.
+    /// This iterator is infinite and always returns some word.
+    fn next(&mut self) -> Option<&'a str> {
+        let i = self.rng.sample(self.distribution);
+        Some(self.words[i])
+    }
+}
+
 #[cfg(test)]
 mod tests {
-    use words;
+    use {words, WordSampler, word_sampler};
+
+    /// How many times to iterate for small or infinite tests.
+    const ITERS: usize = 32;
 
     /// We must have words to use.
     #[test]
@@ -28,9 +84,52 @@ mod tests {
         assert!(!words().is_empty());
     }
 
-    /// We must not have any empty words.
+    /// Words must at least be 3 characters long.
     #[test]
-    fn no_empty_words() {
-        assert!(words().iter().all(|word| !word.is_empty()));
+    fn no_short_words() {
+        assert!(words().iter().all(|word| word.len() >= 3));
+    }
+
+    /// Words must only contain alphabetical characters or a hyphen.
+    #[test]
+    fn all_alpha_hyphen_words() {
+        assert!(
+            words().iter().all(|word|
+                word.chars().all(|c|
+                    c.is_alphabetic() || c == '-'
+                )
+            )
+        );
+    }
+
+    /// Ensure a word sampler is able to produce words.:w
+    /// This test only covers a limited number of checks as the sampler
+    /// itself is infinite.
+    #[test]
+    fn word_sampler_produces() {
+        assert_eq!(
+            word_sampler()
+                .take(ITERS)
+                .count(),
+            ITERS,
+        );
+    }
+
+    /// Ensure a word sampler produces words that are in the word list.
+    /// This test only covers a limited number of checks as the sampler
+    /// itself is infinite.
+    #[test]
+    fn word_sampler_known_words() {
+        // Get a list of words, and build a sampler
+        let words = words();
+        let sampler = WordSampler::new(words.clone());
+
+        assert_eq!(
+            sampler
+                .take(ITERS)
+                .filter(|word| words.contains(word))
+                .count(),
+            ITERS,
+        );
     }
 }
