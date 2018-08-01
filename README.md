a5568982cc2d2788c9e1cfcaddd9cc6f476908e2
timvisee <timvisee@gmail.com>
2018-08-01 23:28:33 +0200

    Update README, add examples

--- README.md
+++ README.md
@@ -1,7 +1,45 @@
 # Correct Horse Battery Staple
-A passphrase generation crate.
+A crate for secure passphrase generation based on a wordlist.
+This library uses cryptographically secure randomization, and may be used
+for generating secret passphrases.
 
 [![xkcd-img]][xkcd]
 
+Notes:
+* this crate is still in development, and should thus be used with care
+* no waranty is provided for the quality of the passwords generated
+  through this library
+
 [xkcd]: https://xkcd.com/936/
 [xkcd-img]: https://imgs.xkcd.com/comics/password_strength.png
+
+## Examples
+Here are some basic examples on how to use this crate.
+
+Generate a passphrase using the helper function consisting of 5 words
+([passphrase.rs](examples/passphrase.rs)):
+
+```rust
+extern crate chbs;
+use chbs::passphrase;
+
+println!("Passphrase: {:?}", passphrase(5));
+```
+
+Use a word sampler to generate an infinite number of random words
+((sampler.rs)[examples/sampler.rs]):
+
+```rust
+extern crate chbs;
+use chbs::word_sampler;
+
+let mut sampler = word_sampler();
+
+for i in 0..8 {
+    println!(
+        "Sampled word #{}: {:?}",
+        i,
+        sampler.next().unwrap(),
+    );
+}
+```
--- src/lib.rs
+++ src/lib.rs
@@ -1,10 +1,23 @@
 //! # Correct Horse Battery Staple
 //! A crate for secure passphrase generation based on a wordlist.
+//! This library uses cryptographically secure randomization, and may be used
+//! for generating secret passphrases.
 //!
-//! ## Generating a passphrase
-//! To generate a secure passphrase, the helper function `passphrase` can be
-//! used. Specify the number of words the passphrase must consist of,
-//! and generate a passphrase string.
+//! [![xkcd-img]][xkcd]
+//! 
+//! Notes:
+//! * this crate is still in development, and should thus be used with care
+//! * no waranty is provided for the quality of the passwords generated
+//!   through this library
+//! 
+//! [xkcd]: https://xkcd.com/936/
+//! [xkcd-img]: https://imgs.xkcd.com/comics/password_strength.png
+//!
+//! ## Examples
+//! Here are some basic examples on how to use this crate.
+//
+//! Generate a passphrase using the helper function consisting of 5 words
+//! ([passphrase.rs](examples/passphrase.rs)):
 //!
 //! ```rust
 //! extern crate chbs;
@@ -13,9 +26,8 @@
 //! println!("Passphrase: {:?}", passphrase(5));
 //! ```
 //!
-//! ## Generate random words
-//! A word sampler can be created to generate an infinite number of
-//! cryptographically secure random words based on the included wordlist.
+//! Use a word sampler to generate an infinite number of random words
+//! ((sampler.rs)[examples/sampler.rs]):
 //!
 //! ```rust
 //! extern crate chbs;
